1598625eeef12515258427806f01dd399be0fc5c
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-01-25 12:20:32 -0800

    Completed rendering To Do items on screen

---
+++ src/components/to_do_list.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ListItem from './list_item';
+
+const toDos = [
+  {
+    id: '01',
+    title: 'Wash car'
+  },
+  {
+    id: '02',
+    title: 'Code more'
+  },
+  {
+    id: '03',
+    title: 'Cook dinner'
+  },
+  {
+    id: '04',
+    title: 'Make money'
+  },
+  {
+    id: '05',
+    title: 'Get paid'
+  }
+]
+
+function ToDoList(props){
+  const toDoElements = toDos.map((item) => {
+    return <ListItem key= {item.id} title={item.title} />
+  });
+  return (
+    <ol>
+      {toDoElements}
+    </ol>
+  )
+}
+
+export default ToDoList;
